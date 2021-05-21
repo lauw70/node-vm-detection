670c9b1aa71da38995f338f08862ab9b4d9bb398
Joost Heitbrink <joostheitbrink@outlook.com>
2021-05-21 19:39:47 +0200

    feat: better typing and docs
    Also use ES6 style arrow functions and proper object structure for module.exports

--- src/index.js
+++ src/index.js
@@ -8,6 +8,76 @@ module.exports = {
      * @property {Number} average - the average value between samples
      * @property {Boolean} isVM - whether the average sample rate was above 1000 (forceVMexit=true) or above 750 (forceVMexit=false)
      * @property {Array} samples - an array containing all the samples that were taken
+    */
+
+    /**
+     * @typedef {Object} CpuInfoResult
+     * @property {String} vendor - the vendor string of the current CPU
+     * @property {String} hypervisorVendor - the vendor string of the hypervisor, if applicable
+     * @property {Boolean} hv_bit - indicates if the current CPU has the hypvervisor bit set
+     * @property {String} brand - the full brand of the current CPU, as reported by the OS
+     * @property {Boolean} knownVMVendor - indicates if the VM vendor is a well known VM vendor
+    */
+
+    /**
+     * @typedef {Object} VMWareResult
+     * @property {Boolean} scsi - 
+     * @property {Boolean} registry -
+     * @property {Boolean} mouseDriver -
+     * @property {Boolean} graphicsDriver -
+     * @property {Boolean} macAddress -
+     * @property {Boolean} devices -
+     * @property {Boolean} wmiSerial -
+    */
+
+    /**
+     * @typedef {Object} VMWareResult
+     * @property {Boolean} scsi - 
+     * @property {Boolean} registry -
+     * @property {Boolean} mouseDriver -
+     * @property {Boolean} graphicsDriver -
+     * @property {Boolean} macAddress -
+     * @property {Boolean} devices -
+     * @property {Boolean} wmiSerial -
+     */
+
+    /**
+     * @typedef {Object} VirtualBoxResult
+     * @property {Boolean} scsi - 
+     * @property {Boolean} biosVersion -
+     * @property {Boolean} guestAdditions -
+     * @property {Boolean} videoBiosVersion -
+     * @property {Boolean} acpi -
+     * @property {Boolean} fadtAcpi -
+     * @property {Boolean} rsdtAcpi -
+     * @property {Boolean} service -
+     * @property {Boolean} systemBiosDate -
+     * @property {Boolean} deviceDrivers -
+     * @property {Boolean} systemFiles -
+     * @property {Boolean} nicMacAddress -
+     * @property {Boolean} trayWindow -
+     * @property {Boolean} sharedNetwork -
+     * @property {Boolean} processes -
+     * @property {Boolean} wmi -
+     */
+
+    /**
+     * @typedef {Object} QEMUResult
+     * @property {Boolean} scsi - 
+     * @property {Boolean} systemBios -
+     * @property {Boolean} cpuBrand -
+     */
+
+     /**
+     * @typedef {Object} WineResult
+     * @property {Boolean} unixFileName - 
+     * @property {Boolean} systemBios -
+     * @property {Boolean} cpuBrand -
+     */
+
+     /**
+     * @typedef {Object} SandboxieResult
+     * @property {Boolean} dll - 
      */
 
     /**
@@ -21,25 +91,32 @@ module.exports = {
      * @returns {RtdscResult}
      * 
      */
-    cpuRdtsc(forceVMexit, numSamples, intervalMs) {
+    cpuRdtsc: (forceVMexit, numSamples, intervalMs) => {
         return binding.cpuRdtsc(forceVMexit, numSamples, intervalMs);
     },
-    cpuInfo() {
-        return binding.cpuInfo();
-    },
-    vmware() {
-        return binding.vmware();
-    },
-    virtualBox() {
-        return binding.virtualBox();
-    },
-    qemu() {
-        return binding.qemu();
-    },
-    wine() {
-        return binding.wine();
-    },
-    sandboxie() {
-        return binding.sandboxie();
-    }
+    /**
+     * @returns {CpuInfoResult}
+     */
+    cpuInfo: () => binding.cpuInfo(),
+    /**
+     * @returns {VMWareResult}
+     */
+    vmware:() => binding.vmware(),
+    /**
+     * @returns {VirtualBoxResult}
+     */
+    virtualBox: () => binding.virtualBox(),
+    /**
+     * @returns {QEMUResult}
+     */
+    qemu: () => binding.qemu(),
+    /**
+     * @returns {WineResult}
+     */
+    wine: () => binding.wine(),
+    /**
+     * @returns {SandboxieResult}
+     */
+    sandboxie: () => binding.sandboxie(),
+    
 };
